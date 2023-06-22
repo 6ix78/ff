ccb6f99ba222d4ba3aacb617cab855745b3a5639
6ix78 <87336515+6ix78@users.noreply.github.com>
2023-06-22 06:51:36 +0100

    Update index.tsx

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-              Join the Crypto Revolution with Alphavars
+                Make Your Crypto Work for You
                 </h1>
                 <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
                Embrace the Future of Crypto Investing, Auto Staking + Trading Simplified!
