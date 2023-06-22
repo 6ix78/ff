815374cd2a0eb1d8696e461f668d62eaa611f13f
6ix78 <87336515+6ix78@users.noreply.github.com>
2023-06-22 20:10:56 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
           {children}
           <Footer />
          <ScrollToTop />
-         <placeholder />
+         <Provider />
       </body>
     </html>
   );
