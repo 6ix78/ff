32424ae013dbd744557fb3f6247f9008ce95919e
6ix78 <87336515+6ix78@users.noreply.github.com>
2023-06-22 19:16:47 +0100

    Update index.tsx

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -22,13 +22,13 @@ const Hero = () => {
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
-                    href="https://alphavars.online/pricing"
+                    href="https://supabase-authentication-6ix78.vercel.app/"
                     className="rounded-md bg-primary py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
                    🔥 Get started today 
                   </Link>
                   <Link
-                    href="https://alphavars.online/signup"
+                    href="https://supabase-authentication-6ix78.vercel.app/"
                     className="rounded-md bg-black/20 py-4 px-8 text-base font-semibold text-black duration-300 ease-in-out hover:bg-black/30 dark:bg-white/20 dark:text-white dark:hover:bg-white/30"
                   >
                     sign in
